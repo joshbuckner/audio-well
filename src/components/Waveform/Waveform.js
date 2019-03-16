1fb1b8d8a81e7561151731e240d6408dc5368c42
Josh Buckner <joshgbuckner@gmail.com>
2019-03-16 18:46:23 -0400

    add notes list, footer audio player, header import button, all mobile first

--- src/components/Waveform/Waveform.js
+++ src/components/Waveform/Waveform.js
@@ -1,67 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
 import './Waveform.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const NUMBER_OF_BUCKETS = 300; // number of "bars" the waveform should have
-const SPACE_BETWEEN_BARS = 0.2; // from 0 (no gaps between bars) to 1 (only gaps - bars won't be visible)
+const seekAudio = (event) => {
+	let startTime = event.target.childNodes[0].innerHTML.slice(1,5);
+	let minutes = Number(startTime.slice(0,1));
+	let seconds = Number(startTime.slice(2,4));
+	const audio = document.getElementById('audio-element');
+	let newCurrentTime = (minutes * 60) + seconds;
 
-let audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-
-const inputAudio = (event) => {
-  var input = event.target.files;
-  document.getElementById('lbl-file-picker').innerHTML = event.target.files[0].name;
-  document.getElementById('audio-source').src = URL.createObjectURL(input[0]);
-  document.getElementById('audio-element').load();
-
-      axios({url: document.getElementById('audio-source').src, responseType: "arraybuffer"}).then(response => {
-      let audioData = response.data;
-
-      audioCtx.decodeAudioData(audioData, buffer => {
-          let decodedAudioData = buffer.getChannelData(0);
-          let bucketDataSize = Math.floor(decodedAudioData.length / NUMBER_OF_BUCKETS);
-          let buckets = [];
-          for (var i = 0; i < NUMBER_OF_BUCKETS; i++) {
-              let startingPoint = i * bucketDataSize;
-              let endingPoint = i * bucketDataSize + bucketDataSize;
-              let max = 0;
-              for (var j = startingPoint; j < endingPoint; j++) {
-                  if (decodedAudioData[j] > max) {
-                      max = decodedAudioData[j];
-                  }
-              }
-              let size = Math.abs(max);
-              buckets.push(size / 2);
-          }
-
-          document.getElementById('waveform-mask').innerHTML = buckets.map((bucket, i) => {
-              let bucketSVGWidth = 100.0 / buckets.length;
-              let bucketSVGHeight = bucket * 150.0;
-
-              return `<rect
-                  x=${bucketSVGWidth * i + SPACE_BETWEEN_BARS / 2.0}
-                  y=${ (100 - bucketSVGHeight) / 2.0}
-                  width=${bucketSVGWidth - SPACE_BETWEEN_BARS}
-                  height=${bucketSVGHeight} />`;
-          }).join('');
-
-          let audioElement = document.getElementById('audio-element');
-          let waveformProgress = document.getElementById('waveform-progress');
-
-          // every 100 milliseconds, update the waveform-progress SVG with a new width - the percentage of time elapsed on the audio file
-          setInterval(() => {
-              waveformProgress.setAttribute('width', audioElement.currentTime / audioElement.duration * 100);
-          }, 100);
-      }, e => {
-          // callback for any errors with decoding audio data
-          console.log('Error with decoding audio data' + e.err);
-      },);
-  }).catch(err => {
-      // catch any errors with fetching the audio
-      console.log(err);
-  });
+	audio.currentTime = newCurrentTime;
+	audio.play();
 }
 
 class Waveform extends Component {
@@ -69,13 +21,7 @@ class Waveform extends Component {
     return (
       <div className="Waveform">
         <Container-fluid>
-          <Row>
-            <Col>
-          		{/* our audio element */}
-          		<label htmlFor="input" id="lbl-file-picker" className="lbl-file-picker">Import Audio</label>
-          		<input onChange={inputAudio} type="file" id="input" className="file-picker"/>
-            </Col>
-          </Row>
+          
         	<Row>
         		<Col>
         			{/* this SVG is the "background" and progress bar */}
@@ -91,14 +37,43 @@ class Waveform extends Component {
           		</svg>
         		</Col>
         	</Row>
-        	<Row>
-        		<Col>
-          		<audio id="audio-element" className="audio-element"controls="controls">
-                <source src="" id="audio-source" />
-              </audio>
-            </Col>
+          <Container>
+          <Row>
+          	<Col>
+          		<div className="notes-container">
+	          		<ul>
+	          			<li onClick={seekAudio} className="list-note oxford-blue">
+	          			<span>(0:49) </span>guitar lead down
+	          			</li>
+	          			<li onClick={seekAudio} className="list-note moonstone-blue">
+	          			<span>(1:37) </span>vocals up
+	          			</li>
+	          			<li onClick={seekAudio} className="list-note fuzz-wuzzy">
+	          			<span>(1:56) </span>remove 2nd half of verse
+	          			</li>
+	          			<li onClick={seekAudio} className="list-note vivid-tangerine">
+	          			<span>(2:10) </span>add post production
+	          			</li>
+	          			<li onClick={seekAudio} className="list-note deep-ruby">
+	          			<span>(2:45) </span>fade out
+	          			</li>
+	          		</ul>
+          		</div>
+          	</Col>
           </Row>
+          </Container>
+          <Container>
+          <Row>
+	          <Col>
+	      			<div className="current-time" id="current-time">0:00</div>
+	      		</Col>
+          	<Col>
+          		<button className="add-note">+</button>
+          	</Col>
+          </Row>
+          </Container>
         </Container-fluid>
+        
       </div>
     );
   }
