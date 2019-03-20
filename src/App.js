85d0c78d0a706374ee8a1e358335e2480c5df683
Josh Buckner <joshgbuckner@gmail.com>
2019-03-20 17:39:18 -0400

    refactor into components

--- src/App.js
+++ src/App.js
@@ -1,83 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAngleDown, faPlus, faTrash, faEdit} from '@fortawesome/free-solid-svg-icons';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Filepicker from './components/Filepicker/Filepicker';
 import Waveform from './components/Waveform/Waveform';
+import Notelist from './components/Notelist/Notelist';
 
 library.add(faAngleDown, faPlus, faTrash, faEdit);
-
-
-
-const NUMBER_OF_BUCKETS = 300; // number of "bars" the waveform should have
-const SPACE_BETWEEN_BARS = 0.2; // from 0 (no gaps between bars) to 1 (only gaps - bars won't be visible)
-
-let audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-
-const inputAudio = (event) => {
-  var input = event.target.files;
-  document.getElementById('lbl-file-picker').innerHTML = event.target.files[0].name;
-  document.getElementById('audio-source').src = URL.createObjectURL(input[0]);
-  document.getElementById('audio-element').load();
-
-  axios({url: document.getElementById('audio-source').src, responseType: "arraybuffer"}).then(response => {
-	  let audioData = response.data;
-
-	  audioCtx.decodeAudioData(audioData, buffer => {
-	    let decodedAudioData = buffer.getChannelData(0);
-	    let bucketDataSize = Math.floor(decodedAudioData.length / NUMBER_OF_BUCKETS);
-	    let buckets = [];
-	    for (var i = 0; i < NUMBER_OF_BUCKETS; i++) {
-	      let startingPoint = i * bucketDataSize;
-	      let endingPoint = i * bucketDataSize + bucketDataSize;
-	      let max = 0;
-	      for (var j = startingPoint; j < endingPoint; j++) {
-	        if (decodedAudioData[j] > max) {
-	        	max = decodedAudioData[j];
-	        }
-	      }
-	      let size = Math.abs(max);
-	      buckets.push(size / 2);
-	    }
-
-	    document.getElementById('waveform-mask').innerHTML = buckets.map((bucket, i) => {
-	      let bucketSVGWidth = 100.0 / buckets.length;
-	      let bucketSVGHeight = bucket * 150.0;
-
-	      return `<rect
-	        x=${bucketSVGWidth * i + SPACE_BETWEEN_BARS / 2.0}
-	        y=${ (100 - bucketSVGHeight) / 2.0}
-	        width=${bucketSVGWidth - SPACE_BETWEEN_BARS}
-	        height=${bucketSVGHeight} />`;
-	    }).join('');
-	    let audioElement = document.getElementById('audio-element');
-	    let waveformProgress = document.getElementById('waveform-progress');
-	    // every 100 milliseconds, update the waveform-progress SVG with a new width - the percentage of time elapsed on the audio file
-	    setInterval(() => {
-	      waveformProgress.setAttribute('width', audioElement.currentTime / audioElement.duration * 100);
-	      let timeDisplay = Math.round(audioElement.currentTime - .6);
-	      let minutes = Math.floor(timeDisplay / 60);
-	      let seconds = timeDisplay % 60;
-	      if (seconds < 10) {
-	      	seconds = "0" + seconds;
-	      }
-	      if (seconds === "0-1") {
-	      	minutes = "0";
-	      	seconds = "00";
-	      }
-	      document.getElementById('current-time').innerHTML = minutes + ":" + seconds;
-	    }, 100);
-	  }, e => {
-	    // callback for any errors with decoding audio data
-	    console.log('Error with decoding audio data' + e.err);
-	  },);
-  }).catch(err => {
-    // catch any errors with fetching the audio
-    console.log(err);
-  });
-}
 
 class App extends Component {
 	constructor(props) {
@@ -107,16 +36,9 @@ class App extends Component {
   render() {
     return (
       <div id="App" className="App">
-	      <div className="fixed-controls-top">
-	      	<Row>
-	      		<Col>
-	        		{/* our audio element */}
-	        		<label htmlFor="input" id="lbl-file-picker" className="lbl-file-picker hvr-push">Import Audio</label>
-	        		<input onChange={inputAudio} type="file" id="input" className="file-picker"/>
-	          </Col>
-	      	</Row>
-	      </div>
-	        <Waveform time={this.state.time}/>
+	      	<Filepicker />
+	        <Waveform />
+	        <Notelist time={this.state.time}/>
       		{/*create audio player component*/}
 	        <div className="audio-player">
         		<audio id="audio-element" className="audio-element"controls="controls">
---
+++ src/components/Notelist/Notelist.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Notelist.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+class Notelist extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {db: [
+				{note: 'Start', time: '0:00', color: 'vivid-tangerine'}
+			],
+			note: 'New Note',
+			counter: 0
+		};
+		this.handleNoteInput = this.handleNoteInput.bind(this);
+	}
+
+	seekAudio = (event) => {
+		let startTime = event.target.parentNode.childNodes[0].innerHTML;
+		let minutes = Number(startTime.slice(0,1));
+		let seconds = Number(startTime.slice(2,4));
+		const audio = document.getElementById('audio-element');
+		let newCurrentTime = (minutes * 60) + seconds;
+
+		audio.currentTime = newCurrentTime;
+		audio.play();
+	}
+
+	expandNote = (event) => {
+	  const toggle = event.currentTarget.parentNode.parentNode.parentNode;
+	  const angleIcon = event.currentTarget;
+	  angleIcon.classList.toggle('rotate');
+	  toggle.classList.toggle('open');
+	}
+
+	createList = () => {
+		let list = [];
+		for (let i = 0; i < this.state.db.length; i++) {
+  		list.push(
+  			<Row key={i}>
+	  			<Col>
+		  			<li className={"list-note " + this.state.db[i].color}>
+			  			<Row>
+			  				<Col style={{textAlign: 'left', overflow: 'hidden'}}>
+									<div className="note-time">{this.state.db[i].time}</div>
+									<div onClick={this.seekAudio} className="note-title">{this.state.db[i].note}</div>
+								</Col>
+								<Col xs="2" sm="2" md="1" lg="1" xl="1" style={{paddingLeft: 0}}>
+			  					<FontAwesomeIcon onClick={this.expandNote} className="note-expand-icon note-expand" icon="angle-down" />
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<div className="note-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ex nulla, sagittis ac lacinia ut, semper a lorem. Sed et condimentum velit, quis lobortis erat. Nulla facilisi. Donec nec lorem sed elit porta rhoncus a quis orci. Donec dignissim ex et faucibus aliquam. </div>
+								</Col>
+								<Col xs="2" sm="2" md="1" lg="1" xl="1" style={{paddingLeft: 0}}>
+									<Row>
+										<Col style={{paddingLeft: '.7rem'}}>
+											<FontAwesomeIcon onClick={this.editNote} className="note-edit-icon" icon="edit" />
+										</Col>
+									</Row>
+									<Row>
+										<Col style={{paddingLeft: '.45rem'}}>
+											<FontAwesomeIcon onClick={this.deleteNote} className="trash-icon" icon="trash"/>
+										</Col>
+									</Row>
+								</Col>
+							</Row>
+		  			</li>
+	  			</Col>
+	  		</Row>
+  		);
+		}
+		return list;
+	}
+
+	createNote = () => {
+		let timeStamp = this.props.time;
+		let colorPallete = ['deep-ruby', 'oxford-blue', 'moonstone-blue', 'fuzz-wuzzy', 'vivid-tangerine'];
+		
+		if (this.state.counter < 4) {
+			this.setState({counter: this.state.counter + 1});
+		} else {
+			this.setState({counter: 0});
+		}
+
+		this.setState({db: this.state.db.concat({note: this.state.note, time: timeStamp, color: colorPallete[this.state.counter]})});
+		document.getElementById('note-input').value = "";
+		this.setState({note: 'New Note'});
+	}
+
+	deleteNote = (event) => {
+		event.currentTarget.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+	}
+	editNote = (event) => {
+		console.log(event.currentTarget);
+		// event.target.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+	}
+
+	handleNoteInput(event) {
+    this.setState({note: event.target.value});
+  }
+
+  handleKeyPress(event) {
+  	if (event.key === 'Enter') {
+  		this.createNote();
+  	}
+  }
+
+	render() {
+    return (
+    	<div className="Notelist">
+	    	<Container>
+	          <Row>
+	          	<Col className="notes-container">
+	          		<ul>
+	          			{this.createList()}
+		          			<Row>
+							  			<Col>
+								  			<li className="list-add-note dark-theme">
+								  			<Row>
+								  				<Col style={{textAlign: 'left'}}>
+														<div className="current-time" id="current-time">{this.props.time}</div>
+														<input placeholder="New Note" id="note-input" type="text" onChange={this.handleNoteInput} onKeyPress={this.handleKeyPress.bind(this)}/>
+													</Col>
+													<Col xs="2" sm="2" md="1" lg="1" xl="1" style={{paddingLeft: 0}}>
+														<div onClick={this.createNote} className="add-note">
+								  						<FontAwesomeIcon className="note-add-icon" icon="plus" />
+														</div>
+													</Col>
+												</Row>
+								  			</li>
+							  			</Col>
+							  		</Row>
+	          		</ul>
+	          	</Col>
+	          </Row>
+          </Container>
+      </div>
+    );
+  }
+}
+
+export default Notelist;
